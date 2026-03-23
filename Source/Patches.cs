2b259283d2696b6cfda730035dd5737134fe9d24
PaperclipCereal <paperclipcereal@gmail.com>
2026-03-22 23:18:11 -0400

    Fixed LOS bug for Achtung, Yayo 3, Legion

--- Source/Patches.cs
+++ Source/Patches.cs
@@ -6,17 +6,30 @@ using Verse.AI;
 
 namespace CommsHeadset
 {
+    public static class RadioSocialState
+    {
+        [ThreadStatic]
+        public static bool IsSocialActive;
+    }
+
+    [HarmonyPatch(typeof(Pawn), nameof(Pawn.SpawnSetup))]
+    public static class Patch_Pawn_SpawnSetup_RadioSync
+    {
+        public static void Postfix(Pawn __instance, Map map)
+        {
+            if (__instance.apparel == null || map == null) return;
+
+            map.GetComponent<MapComponent_CommNetwork>()?.Notify_ApparelChanged(__instance);
+        }
+    }
+
+    // --- APPAREL TRACKING ---
     [HarmonyPatch(typeof(Pawn_ApparelTracker), nameof(Pawn_ApparelTracker.Notify_ApparelAdded))]
     public static class Patch_ApparelAdded
     {
         public static void Postfix(Pawn_ApparelTracker __instance)
         {
-            Pawn pawn = __instance.pawn;
-            if (pawn.MapHeld == null) return;
-
-            pawn.MapHeld
-                .GetComponent<MapComponent_CommNetwork>()
-                ?.Notify_ApparelChanged(pawn);
+            __instance.pawn.MapHeld?.GetComponent<MapComponent_CommNetwork>()?.Notify_ApparelChanged(__instance.pawn);
         }
     }
 
@@ -25,87 +38,66 @@ namespace CommsHeadset
     {
         public static void Postfix(Pawn_ApparelTracker __instance)
         {
-            Pawn pawn = __instance.pawn;
-            if (pawn.MapHeld == null) return;
-
-            pawn.MapHeld
-                .GetComponent<MapComponent_CommNetwork>()
-                ?.Notify_ApparelChanged(pawn);
+            __instance.pawn.MapHeld?.GetComponent<MapComponent_CommNetwork>()?.Notify_ApparelChanged(__instance.pawn);
         }
     }
 
     [HarmonyPatch(typeof(Pawn), nameof(Pawn.DeSpawn))]
     public static class Patch_PawnDeSpawn
     {
-        public static void prefix(Pawn __instance)
+        public static void Prefix(Pawn __instance)
         {
-            if (__instance.MapHeld == null) return;
-
-            __instance.MapHeld
-                .GetComponent<MapComponent_CommNetwork>()
-                ?.Notify_PawnDespawned(__instance);
+            __instance.MapHeld?.GetComponent<MapComponent_CommNetwork>()?.Notify_PawnDespawned(__instance);
         }
     }
+
     [HarmonyPatch(typeof(Pawn_InteractionsTracker), nameof(Pawn_InteractionsTracker.CanInteractNowWith), new[] { typeof(Pawn), typeof(InteractionDef) })]
-    public static class Patch_CanInteractWith
+    public static class Patch_CanInteractWith_Unified
     {
-        public static bool Prefix(Pawn_InteractionsTracker __instance, Pawn ___pawn, Pawn recipient, ref bool __result)
+        [HarmonyPrefix]
+        public static bool Prefix(Pawn ___pawn, Pawn recipient, InteractionDef interactionDef, ref bool __result)
         {
-            if (___pawn?.MapHeld == null || recipient?.MapHeld == null)
+            if (___pawn?.MapHeld == null || recipient?.MapHeld == null || ___pawn.MapHeld != recipient.MapHeld)
                 return true;
 
-            if (___pawn.MapHeld != recipient.MapHeld)
-                return true;
-
-            if (___pawn.Faction == null || ___pawn.Faction != recipient.Faction)
-                return true;
-
-            MapComponent_CommNetwork commHeadNetwork = ___pawn.MapHeld.GetComponent<MapComponent_CommNetwork>();
-            if (commHeadNetwork == null)
-                return true;
-
-            if (commHeadNetwork.IsOnNetwork(___pawn) && commHeadNetwork.IsOnNetwork(recipient))
+            var network = ___pawn.MapHeld.GetComponent<MapComponent_CommNetwork>();
+            if (network != null && network.IsOnNetwork(___pawn) && network.IsOnNetwork(recipient))
             {
+                RadioSocialState.IsSocialActive = true;
                 __result = true;
                 return false;
             }
-
             return true;
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            RadioSocialState.IsSocialActive = false;
         }
     }
 
-    [HarmonyPatch(typeof(GenSight), nameof(GenSight.LineOfSight), new[] { typeof(IntVec3), typeof(IntVec3), typeof(Map), typeof(bool), typeof(Func<IntVec3, bool>), typeof(int), typeof(int) })]
+    [HarmonyPatch(typeof(GenSight), nameof(GenSight.LineOfSight))]
+    [HarmonyPatch(new[] { typeof(IntVec3), typeof(IntVec3), typeof(Map), typeof(bool), typeof(Func<IntVec3, bool>), typeof(int), typeof(int) })]
     public static class Patch_RadioLineOfSight
     {
-        public static void Postfix(ref bool __result, IntVec3 start, IntVec3 end, Map map)
+        public static void Postfix(ref bool __result)
         {
-            if (__result) return;
-
-            Pawn pawn = start.GetFirstPawn(map);
-            Pawn recipient = end.GetFirstPawn(map);
-
-            if (pawn != null && recipient != null)
-            {
-                var commHeadNetwork = map.GetComponent<MapComponent_CommNetwork>();
-                if (commHeadNetwork != null && commHeadNetwork.IsOnNetwork(pawn) && commHeadNetwork.IsOnNetwork(recipient))
-                {
-                    __result = true;
-                }
-            }
+            if (RadioSocialState.IsSocialActive) __result = true;
         }
     }
 
     [HarmonyPatch(typeof(ReachabilityImmediate), nameof(ReachabilityImmediate.CanReachImmediate), new[] { typeof(Pawn), typeof(LocalTargetInfo), typeof(PathEndMode) })]
     public static class Patch_RadioReachability
     {
-        public static void Postfix(ref bool __result, Pawn pawn, LocalTargetInfo target, PathEndMode peMode)
+        public static void Postfix(ref bool __result, Pawn pawn, LocalTargetInfo target)
         {
             if (__result) return;
 
             if (target.Thing is Pawn recipient && pawn.MapHeld != null)
             {
-                var commHeadNetwork = pawn.MapHeld.GetComponent<MapComponent_CommNetwork>();
-                if (commHeadNetwork != null && commHeadNetwork.IsOnNetwork(pawn) && commHeadNetwork.IsOnNetwork(recipient))
+                var network = pawn.MapHeld.GetComponent<MapComponent_CommNetwork>();
+                if (network != null && network.IsOnNetwork(pawn) && network.IsOnNetwork(recipient))
                 {
                     __result = true;
                 }
